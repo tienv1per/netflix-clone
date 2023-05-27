ddeaf9784a1129ad30be609489584a389c942d59
tienv1per <tienv1perds@gmail.com>
2023-05-28 01:06:01 +0700

    add featured movies components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import {AcUnit} from "@mui/icons-material";
 import "./home.scss";
 import Navbar from '../../components/navbar/Navbar';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
     return (
         <div className='home'>
-            <Navbar/>
-            <img 
-                width="100%"
-                src='https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-                alt=''
-            />            
+            <Navbar/> 
+            <Featured type="movie"/>         
         </div>
     )
 }
